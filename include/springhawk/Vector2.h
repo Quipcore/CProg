0a7dc463d227d9dfcfbf2404907c32d80aa67411
Felix Lidö <felix.lido@gmail.com>
2023-11-26 16:33:14 +0100

    Commented and expanded on the Vector2 class

--- include/springhawk/Vector2.h
+++ include/springhawk/Vector2.h
@@ -10,22 +10,143 @@
 class Vector2{
 public:
     Vector2(double x, double y);
+    Vector2(int x, int y);
+    Vector2(const Vector2& rhs);
     Vector2();
 
-    Vector2(int x, int y);
+    /**
+     * Shorhand for Vector2(0,0)
+     */
+    static const Vector2 zero;
+
+    /**
+     * Shorthand for Vector2(1,1)
+     */
+    static const Vector2 one;
+
+    /**
+     * Shorthand for Vector2(0,1)
+     */
+    static const Vector2 up;
+
+    /**
+     * Shorthand for Vector2(0,-1)
+     */
+    static const Vector2 down;
+
+    /**
+     * Shorthand for Vector2(-1,0)
+     */
+    static const Vector2 left;
+
+    /**
+     * Shorthand for Vector2(1,0)
+     */
+    static const Vector2 right;
+
+    /**
+     * Returns the value of the x component of this vector
+     * @return The x value of this vector
+     */
+    double getX() const;
+
+    /**
+     * Returns the value of the y component of this vector
+     * @return The y value of this vector
+     */
+    double getY() const;
 
     Vector2 operator+(const Vector2& rhs) const;
     Vector2 operator-(const Vector2& rhs) const;
     Vector2 operator*(const double& rhs) const;
     Vector2 operator+=(const Vector2& rhs);
     Vector2 operator-=(const Vector2& rhs);
-    friend std::ostream& operator<<(std::ostream& os, const Vector2& dt);
+    Vector2 operator*=(const double& rhs);
     Vector2& operator=(const Vector2& rhs);
+    bool operator==(const Vector2& rhs) const;
 
-    double getX() const {return x;}
-    double getY() const {return y;}
+    friend std::ostream& operator<<(std::ostream& os, const Vector2& dt);
 
+    /**
+     * Returns the magnitude (length) of this vector
+     * @return The magnitude (length) of this vector
+     */
     double magnitude() const;
+
+    /**
+     * Returns the dot product of this vector and the given vector
+     * @param rhs The vector to compare this vector to
+     * @return Dot product of this vector and the given vector
+     */
+    double dot(const Vector2& rhs) const;
+
+    /**
+     * Returns the distance between this vector and the given vector
+     * @param rhs The vector to compare this vector to
+     * @return The distance between this vector and the given vector
+     */
+    double distance(const Vector2& rhs) const;
+
+    /**
+     * Returns the angle between this vector and the given vector. Given in radians
+     * @param rhs The vector to compare this vector to
+     * @return The angle between this vector and the given vector in radians
+     */
+    double angleBetween(const Vector2& rhs) const;
+
+    /**
+     * Returns a normalized version of this vector
+     * @return A normalized version of this vector
+     */
+    Vector2 normalize();
+
+    /**
+     * Rotates this vector by the given angle
+     * @param angle The angle to rotate this vector by
+     * @return The rotated vector
+     */
+    Vector2 rotate(double angle);
+
+    /**
+     * Returns the dot product of the two given vectors. Order does not matter.
+     * @param v1 First vector
+     * @param v2 Second vector
+     * @return Dot product of the two given vectors
+     */
+    static double dot(const Vector2& v1, const Vector2& v2);
+
+    /**
+     * Returns the distance between the two given vectors
+     * @param v1 First vector
+     * @param v2 Second vector
+     * @return Distance between the two given vectors
+     */
+    static double distance(const Vector2& v1, const Vector2& v2);
+
+    /**
+     * Returns the angle between the two given vectors. Given in radians
+     * @param v1 First vector
+     * @param v2 Second vector
+     * @return Angle between the two given vectors in radians
+     */
+    static double angle(const Vector2& v1, const Vector2& v2);
+
+    /**
+     * Returns a normalized version of the given vector
+     * @param vector
+     * @return Normal vector. That is, a vector with the same direction but with magnitude (length) 1
+     */
+    static Vector2 normalized(const Vector2& vector);
+
+    /**
+     * Returns a rotated version of the given vector
+     * @param vector Vector to rotate
+     * @param angle Angle to rotate the vector by
+     * @return Rotated vector
+     */
+    static Vector2 rotated(const Vector2& vector, double angle);
+
+
 
 private:
     double y;
--- src/springhawk/Vector2.cpp
+++ src/springhawk/Vector2.cpp
@@ -4,6 +4,39 @@
 #include <cmath>
 #include "springhawk/Vector2.h"
 
+//Unit vector is up and right, that is (0,1) and (1,0). This is to make it easier for the user to work with the engine.
+//The origin is in the lower left corner of the screen.
+Vector2 const Vector2::zero = Vector2(0,0);
+Vector2 const Vector2::one = Vector2(1,1);
+Vector2 const Vector2::up = Vector2(0,1);
+Vector2 const Vector2::down = Vector2(0,-1);
+Vector2 const Vector2::left = Vector2(-1,0);
+Vector2 const Vector2::right = Vector2(1,0);
+
+
+Vector2::Vector2(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
+
+Vector2::Vector2(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+Vector2::Vector2() {
+    x = 0;
+    y = 0;
+}
+
+double Vector2::getX() const {
+    return x;
+}
+
+double Vector2::getY() const {
+    return y;
+}
+
 Vector2 Vector2::operator+(const Vector2 &rhs) const {
     return {x+rhs.x,y+rhs.y};
 }
@@ -33,19 +66,10 @@ Vector2& Vector2::operator=(const Vector2& rhs){
     return *this;
 }
 
-Vector2::Vector2(int x, int y) {
-    this->x = x;
-    this->y = y;
-}
-
-Vector2::Vector2(double x, double y) {
-    this->x = x;
-    this->y = y;
-}
-
-Vector2::Vector2() {
-    x = 0;
-    y = 0;
+Vector2 Vector2::operator*=(const double &rhs) {
+    x *= rhs;
+    y *= rhs;
+    return *this;
 }
 
 std::ostream &operator<<(std::ostream &os, const Vector2 &dt) {
@@ -55,4 +79,66 @@ std::ostream &operator<<(std::ostream &os, const Vector2 &dt) {
 
 double Vector2::magnitude() const {
     return std::sqrt((x*x) + (y*y));
+}
+
+double Vector2::dot(const Vector2 &v1, const Vector2 &v2) {
+    return v1.x*v2.x + v1.y*v2.y;
+}
+
+double Vector2::distance(const Vector2 &v1, const Vector2 &v2) {
+    return (v1 - v2).magnitude();
+}
+
+double Vector2::angle(const Vector2 &v1, const Vector2 &v2) {
+    double magProduct = v1.magnitude() * v2.magnitude();
+    if(magProduct != 0){
+        double dotProduct = Vector2::dot(v1,v2);
+        return std::acos(dotProduct/magProduct);
+    }
+    return 0;
+}
+
+Vector2 Vector2::normalized(const Vector2 &vector) {
+    return vector * (1 / vector.magnitude());
+}
+
+Vector2 Vector2::rotated(const Vector2 &vector, double angle) {
+    double newX = vector.x * std::cos(angle) - vector.y * std::sin(angle);
+    double newY = vector.x * std::sin(angle) + vector.y * std::cos(angle);
+    return {newX, newY};
+}
+
+bool Vector2::operator==(const Vector2 &rhs) const {
+    return x == rhs.x && y == rhs.y;
+}
+
+Vector2::Vector2(const Vector2 &rhs) {
+    x = rhs.x;
+    y = rhs.y;
+}
+
+double Vector2::dot(const Vector2 &rhs) const {
+    return (this->x*rhs.x) + (this->y*rhs.y);
+}
+
+double Vector2::angleBetween(const Vector2 &rhs) const {
+    double dot = this->dot(rhs);
+    double mag = this->magnitude() * rhs.magnitude();
+    return std::acos(dot/mag);
+}
+
+Vector2 Vector2::normalize() {
+    *this *= 1/this->magnitude();
+    return *this;
+}
+
+Vector2 Vector2::rotate(double angle){
+    double newX = x * std::cos(angle) - y * std::sin(angle);
+    double newY = x * std::sin(angle) + y * std::cos(angle);
+    *this = Vector2(newX, newY);
+    return *this;
+}
+
+double Vector2::distance(const Vector2 &rhs) const {
+    return (*this - rhs).magnitude();
 }
