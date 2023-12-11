7e35a387479e04fa0fb7d384b73ca6e727ceb0d9
Felix Lidö <felix.lido@gmail.com>
2023-12-11 18:23:37 +0100

    Rendertag were apperently usefull

---
+++ include/springhawk/renderers/RenderTag.h
@@ -0,0 +1,16 @@
+//
+// Created by felix on 2023-11-16.
+//
+
+#ifndef CPROG_PROJECT_RENDERTAGH_H
+#define CPROG_PROJECT_RENDERTAGH_H
+
+namespace springhawk{
+    enum RenderTag{
+        Plane,
+        Raycaster,
+        Doom
+    };
+}
+
+#endif //CPROG_PROJECT_RENDERTAGH_H
