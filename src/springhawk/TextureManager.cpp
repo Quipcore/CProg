f82ca7fe26142381d1ed5d994ebe400fe1a6daef
Felix Lidö <felix.lido@gmail.com>
2024-01-13 16:30:20 +0100

    Movement now works

--- src/springhawk/TextureManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "springhawk/TextureManager.h"
-
-TextureManager::TextureManager(std::initializer_list<std::string> paths) {
-    for (auto &path : paths) {
-        if(textures.find(path) == textures.end()){
-            this->textures[path] = nullptr;
-        }
-    }
-}
